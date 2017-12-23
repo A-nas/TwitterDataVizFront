7db20e7c3b4eb30d6a34f81a0ed390d4864d915d
A-nas <anass.laghouaouta@hotmail.fr>
2017-12-23 06:50:15 +0100

    add all main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ChartSwapperComponent } from './chart-swapper/chart-swapper.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChartSwapperComponent
   ],
   imports: [
     BrowserModule
