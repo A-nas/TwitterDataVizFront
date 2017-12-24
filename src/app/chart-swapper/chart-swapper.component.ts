61117ea26604340ed5697b7095456507e435b7fc
A-nas <anass.laghouaouta@hotmail.fr>
2017-12-25 00:07:09 +0100

    add all component to index

---
+++ src/app/chart-swapper/chart-swapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-swapper',
+  templateUrl: './chart-swapper.component.html',
+  styleUrls: ['./chart-swapper.component.css']
+})
+export class ChartSwapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/d3wordcloud/d3wordcloud.component.ts
@@ -0,0 +1,66 @@
+import { Component, ElementRef, NgZone, OnDestroy, OnInit } from '@angular/core';
+
+import {
+  D3Service,
+  D3,
+  Axis,
+  BrushBehavior,
+  BrushSelection,
+  D3BrushEvent,
+  ScaleLinear,
+  ScaleOrdinal,
+  Selection,
+  Transition
+} from 'd3-ng2-service';
+
+@Component({
+  selector: 'app-d3wordcloud',
+  templateUrl: './d3wordcloud.component.html',
+  styleUrls: ['./d3wordcloud.component.css']
+})
+export class D3wordcloudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    var d3 = require("d3"),
+    cloud = require("../");
+
+var fill = d3.scale.category20();
+
+var layout = cloud()
+    .size([500, 500])
+    .words([
+      "Hello", "world", "normally", "you", "want", "more", "words",
+      "than", "this"].map(function(d) {
+      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+    }))
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+
+layout.start();
+
+function draw(words) {
+  d3.select("#wordcloud").append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+    }
+  }
+
+}
