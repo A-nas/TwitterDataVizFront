1925c5098b5d6294529a73e582a93c8d4df7eba4
A-nas <anass.laghouaouta@hotmail.fr>
2018-01-09 02:20:27 +0100

    add service get method and fix footer large height data issue

--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core'
 import {Http} from '@angular/http'
 @Injectable()
 export class StatsService{
-    constructor(private http: Http){
-        
+    constructor(private http: Http){}
+    getTopTweets(){
+        this.http.get('http://127.0.0.1:8000');
     }
 }
