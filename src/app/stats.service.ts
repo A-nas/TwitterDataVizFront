3b1d6b00110e95d347c7c707d3c4c291a0962232
A-nas <anass.laghouaouta@hotmail.fr>
2018-01-31 02:12:05 +0100

    add toptweetpermonth and toptweetbyyear to stats service

--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -4,13 +4,29 @@ import {Http} from '@angular/http'
 export class StatsService{
 
     constructor(private http: Http){}
-
-    // get top 50 hashtag, mentions and words
+    
+    /**
+     * get top 50 hashtag, mentions and words
+     * str param can take as parameter {"Hashtag","Mention","Word"}
+     * used in wordcloudComponent
+     */
     getTopTweets(str: string){
         return this.http.get('http://127.0.0.1:8000/topwords/'.concat(str));
         }
 
+    /**
+     * get count of tweets by day
+     * used in LineGraphComponent
+     */
     getNTweetByDay(){
         return this.http.get('http://127.0.0.1:8000/toptweetperday');
+    }
+
+    getNTweetByYear(){
+        return this.http.get('http://127.0.0.1:8000/toptweetperyear');
+    }
+
+    getNTweetByMonth(){
+        return this.http.get('http://127.0.0.1:8000/toptweetpermonth');
     }
 }
